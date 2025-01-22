632971389fb506de00b5155cdffd9ece3174ef9c
杨再涛 <1772081518@qq.com>
2025-01-22 09:44:35 +0800

    添加代码

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.module.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Server启动");
+        Socket socket = new Socket();
+        socket.start();
+    }
+}
