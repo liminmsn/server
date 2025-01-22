a2881f3b836e44a1218aaa08ebac8c80dc825311
杨再涛 <1772081518@qq.com>
2025-01-22 14:11:42 +0800

    websocket搭建完成

---
+++ src/main/java/org/example/module/SocketServer.java
@@ -0,0 +1,56 @@
+package org.example.module;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+public class SocketServer extends WebSocketServer {
+
+    public SocketServer(int port) throws UnknownHostException {
+        super(new InetSocketAddress(port));
+    }
+
+    public SocketServer(InetSocketAddress address) {
+        super(address);
+    }
+
+    @Override
+    public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+        webSocket.send("Welcome to the server!"); // This method sends a message to the new client
+        broadcast("new connection: " + clientHandshake
+                .getResourceDescriptor()); // This method sends a message to all clients connected
+        System.out.println(
+                webSocket.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!");
+    }
+
+    @Override
+    public void onClose(WebSocket webSocket, int i, String s, boolean b) {
+        broadcast(webSocket + " has left the room!");
+        System.out.println(webSocket + " has left the room!");
+    }
+
+    @Override
+    public void onMessage(WebSocket webSocket, String s) {
+        broadcast(s);
+        System.out.println(webSocket + ": " + s);
+    }
+
+    @Override
+    public void onError(WebSocket webSocket, Exception e) {
+        e.printStackTrace();
+        if (webSocket != null) {
+            // some errors like port binding failed may not be assignable to a specific
+            // websocket
+        }
+    }
+
+    @Override
+    public void onStart() {
+        System.out.println("Server started!");
+        setConnectionLostTimeout(0);
+        setConnectionLostTimeout(100);
+    }
+}
